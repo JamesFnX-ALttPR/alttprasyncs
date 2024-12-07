137e20e0ff7a23882c265eede34b1f8956d137c1
James <139070656+JamesFnX-ALttPR@users.noreply.github.com>
2024-12-07 11:40:51 -0800

    Adding js validation to register form

--- src/inputNewUser.php
+++ src/inputNewUser.php
@@ -1,24 +1,64 @@
-<?php
-
-echo '        <form method="post" autocomplete="off" action="' . $domain . '/register">' . PHP_EOL;
-echo '            <table class="register">' . PHP_EOL;
-echo '                <thead>' . PHP_EOL;
-echo '                    <caption>Register Account</caption>' . PHP_EOL;
-echo '                </thead>' . PHP_EOL;
-echo '                <tbody>' . PHP_EOL;
-echo '                    <tr><td><label for="email">Email: </label></td><td><input type="text" id="email" name="email" placeholder="you@domain.com" required ';
-if (isset($_POST['email'])) {
-    echo 'value="' . $_POST['email'] . '"';
-}
-echo '/></td></tr>' . PHP_EOL;
-echo '                    <tr><td><label for="displayName">Display Name: </label></td><td><input type="text" id="displayName" name="displayName" required ';
-if (isset($_POST['displayName'])) {
-    echo 'value="' . $_POST['displayName'] . '"';
-}
-echo '/></td></tr>' . PHP_EOL;
-echo '                    <tr><td><label class="passwordLabel" for="password1" title="Passwords must be at least eight characters. Passwords less than 16 characters must include uppercase, lowercase, numeric, and special characters.">Password: </label></td><td><input type="password" id="password1" name="password1" required /></td></tr>' . PHP_EOL;
-echo '                    <tr><td><label for="password2">Confirm Password: </label></td><td><input type="password" id="password2" name="password2" required /></td></tr>' . PHP_EOL;
-echo '                    <tr><td colspan="2" class="submitButton"><input type="Submit" class="submitButton" value="Register" />' . PHP_EOL;
-echo '                </tbody>' . PHP_EOL;
-echo '            </table>' . PHP_EOL;
-echo '        </form>' . PHP_EOL;
+        <form method="post" autocomplete="off" action="<?= $domain ?>/register">
+            <table class="register">
+                <caption>Register Account</caption>
+                <tbody>
+                    <tr>
+                        <td>
+                            <label for="email">Email: </label>
+                        </td>
+                        <td>
+                            <input type="text" id="email" name="email" placeholder="you@domain.com" required <?php if (isset($_POST['email'])) { echo 'value="' . $_POST['email'] . '"'; } ?>/>
+                        </td>
+                    </tr>
+                    <tr id="validEmail" style="display: none;">
+                        <td colspan="2">
+                            <span class="invalid">Please enter a valid email address.</span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label for="displayName">Display Name: </label>
+                        </td>
+                        <td>
+                            <input type="text" id="displayName" name="displayName" required <?php if (isset($_POST['displayName'])) { echo 'value="' . $_POST['displayName'] . '"'; } ?>/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label for="password">Password: </label></td><td><input type="password" id="password" name="password" required />
+                        </td>
+                    </tr>
+                    <tr id="passwordRequirements" style="display: none;">
+                        <td colspan="2">
+                            <h4>Passwords must contain the following:</h4>
+                            <span id="lower">At least 1 lowercase letter</span><br />
+                            <span id="upper">At least 1 uppercase letter</span><br />
+                            <span id="number">At least 1 number</span><br />
+                            <span id="special">At least 1 special character</span><br />
+                            <span id="length">At least 8 characters</span><br />
+                            <h4>Or; Password must be:</h4>
+                            <span id="longer">At least 16 characters</span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label for="confirmPassword">Confirm Password: </label>
+                        </td>
+                        <td>
+                            <input type="password" id="confirmPassword" name="confirmPassword" required />
+                        </td>
+                    </tr>
+                    <tr id="matchingPassword" style="display: none;">
+                        <td colspan="2">
+                            <span class="invalid">Passwords do not match!</span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" class="submitButton">
+                            <input type="Submit" id="submitButton" class="submitButton" value="Register" disabled />
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </form>
+    <script src="<?= $domain ?>/includes/passwordVerification.js"></script>

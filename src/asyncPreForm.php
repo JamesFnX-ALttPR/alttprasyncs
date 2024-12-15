ffdd3825f23bd5329ffa39562ac8f237e42cb6a1
James <139070656+JamesFnX-ALttPR@users.noreply.github.com>
2024-12-15 13:55:01 -0800

    Updating queries and moving code to more HTML with PHP inserts

--- src/asyncPreForm.php
+++ src/asyncPreForm.php
@@ -1,28 +1,30 @@
 <?php
-$stmt = $pdo->prepare('SELECT count(1) FROM results WHERE raceSlug = ?');
-$stmt->execute([$raceSlug]);
+$stmt = $pdo->prepare("SELECT count(1) FROM results WHERE raceSlug = :slug");
+$stmt->bindParam(':slug', $raceSlup, PDO::PARAM_STR);
+$stmt->execute();
 $racerCount = $stmt->fetchColumn();
 $url = 'https://sahasrahbotapi.synack.live/presets/api/alttpr?preset=' . $raceMode;
 $data = curlData($url);
 $raceModeDesc = parseSynackAPI($data);
-$stmt = $pdo->prepare("SELECT name, description FROM modes WHERE name = ?");
-$stmt->execute([$raceMode]);
+$stmt = $pdo->prepare("SELECT name, description FROM modes WHERE name = :mode");
+$stmt->bindParam(':mode', $raceMode, PDO::PARAM_STR);
+$stmt->execute();
 $rslt = $stmt->fetch();
 if(! $rslt) {
-    $sql = 'INSERT INTO modes (name, description) VALUES (?, ?)';
-    $pdo->prepare($sql)->execute([$raceMode, $raceModeDesc]);
+    $stmt2 = $pdo->prepare("INSERT INTO modes (name, description) VALUES (:mode, :desc)");
+    $stmt2->bindParam(':mode', $raceMode, PDO::PARAM_STR);
+    $stmt2->bindParam(':desc', $raceModeDesc, PDO::PARAM_STR);
+    $stmt2->execute();
 } else {
-    $sql = 'UPDATE modes SET description = ? WHERE name = ?';
-    $pdo->prepare($sql)->execute([$raceModeDesc, $raceMode]);
+    $stmt2 = $pdo->prepare("UPDATE modes SET description = :desc WHERE name = :mode");
+    $stmt2->bindParam(':desc', $raceModeDesc, PDO::PARAM_STR);
+    $stmt2->bindParam(':mode', $raceMode, PDO::PARAM_STR);
+    $stmt2->execute();
 }
-echo '        <div class="asyncTopRow">Submit Async for ';
-if($raceFromRacetime == 'y') {
-    echo '<a target="_blank" href="https://racetime.gg/alttpr/' . $raceSlug . '">' . $raceSlug . '</a>';
-} else {
-    echo $raceSlug;
-}
-echo '</div><br />' . PHP_EOL;
-echo '        <div class="asyncMiddle">Mode: ' . $raceMode . '<br />' . PHP_EOL;
+?>
+        <div class="asyncTopRow">Submit Async for <?php if($raceFromRacetime == 'y') { echo '<a target="_blank" href="https://racetime.gg/alttpr/' . $raceSlug . '">' . $raceSlug . '</a>'; } else { echo $raceSlug; } ?></div><br />
+        <div class="asyncMiddle">Mode: <?= $raceMode ?><br />
+<?php
 if($raceIsTeam == 'y') {
     if ($raceDescription == '') {
         $raceDescription = 'CO-OP/TEAM';
@@ -37,17 +39,19 @@ if($raceIsSpoiler == 'y') {
         $raceDescription = $raceDescription . ' - <a target="_blank" href="' . $raceSpoilerLink . '">Download Spoiler Log</a>';
     }
 }
-echo '        ' . $raceDescription . '<br />' . PHP_EOL;
-echo '        Seed Link - <a target="_blank" href="' . $raceSeed . '">' . $raceSeed . '</a> - Hash: ' . hashToImages($raceHash) . '<br />' . PHP_EOL;
-$stmt = $pdo->prepare('SELECT racetimeName FROM racerinfo WHERE racetimeID IN (SELECT racerRacetimeID FROM results WHERE raceSlug = ?) ORDER BY racetimeName');
+?>
+        <?php if ($raceDescription != '') { echo '<br />'; } ?>
+        Seed Link - <a target="_blank" href="<?= $raceSeed ?>"><?= $raceSeed ?></a> - Hash: <?php echo hashToImages($raceHash); ?><br />
+<?php $stmt = $pdo->prepare('SELECT racetimeName FROM racerinfo WHERE racetimeID IN (SELECT racerRacetimeID FROM results WHERE raceSlug = ?) ORDER BY racetimeName');
 $stmt->execute([$raceSlug]);
 $racerList = '';
 while($row = $stmt->fetch()) {
     $racerList = $racerList . $row['racetimeName'] . ', ';
 }
 $racerList = substr($racerList, 0, -2);
-echo '        Participants: ' . $racerList . '</div>' . PHP_EOL;
-echo '        <hr />' . PHP_EOL;
-echo '        <form action="' . $domain . '/async/' . $raceID .'" method="post" autocomplete="off">' . PHP_EOL;
-echo '        <table class="submitAsync">' . PHP_EOL;
-echo '            <caption>Submit Your Time';
+?>
+        Participants: <?= $racerList ?></div>
+        <hr />
+        <form action="<?= $domain ?>/async/<?= $raceID ?>" method="post" autocomplete="off">
+            <table class="submitAsync">
+                <caption>Submit Your Time

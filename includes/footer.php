e953120e161e1e3df64c73dfd7fb73d7c6e23dfa
James <139070656+JamesFnX-ALttPR@users.noreply.github.com>
2025-02-08 15:51:00 -0800

    Adding Ko-fi button to footer

--- includes/footer.php
+++ includes/footer.php
@@ -2,10 +2,10 @@
 
 echo '        <hr />' . PHP_EOL;
 if (isset($_SESSION['userid'])) {
-    echo '        <div><span class="footerleft">Logged in as ' . $_SESSION['displayName'] . '</span>';
+    echo '        <div class="footer"><span class="footerleft">Logged in as ' . $_SESSION['displayName'] . '</span>';
 } else {
     echo '        <div>';
 }
-echo '<span class="footerright">' . date("Y") . ' - Created by <a target="_blank" href="https://twitch.tv/jamesfnx">JamesFnX</a></span></div>' . PHP_EOL;
+echo '<span><script type="text/javascript" src="https://storage.ko-fi.com/cdn/widget/Widget_2.js"></script><script type="text/javascript">kofiwidget2.init("Tip JamesFnX", "#000000", "B0B6156Z29");kofiwidget2.draw();</script></span><span class="footerright">' . date("Y") . ' - Created by <a target="_blank" href="https://twitch.tv/jamesfnx">JamesFnX</a></span></div>';
 echo '    </body>' . PHP_EOL;
 echo '</html>' . PHP_EOL;
